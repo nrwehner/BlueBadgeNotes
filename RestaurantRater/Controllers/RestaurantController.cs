fbd22ef18997032f59158473aef8db14bb724e01
Nick Wehner <nrwehner@gmail.com>
2020-02-25 14:07:22 -0500

    added endpoints

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -24,5 +25,36 @@ namespace RestaurantRater.Controllers
             }
             return BadRequest(ModelState);
         }//as soon as we call this method for the first time, our database will be scaffolded out, up until then, all we have is a dbcontext(stagin area)
+
+        //GET ALL
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);//sending the content along with the ok result
+        }
+
+        //GET BY ID
+        public async Task<IHttpActionResult> GetById(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+            if(restaurant is null)
+            {
+            return NotFound();
+            }
+            return Ok(restaurant);
+        }
+        public async Task<IHttpActionResult> GetByDollarSigns(int dollarSigns)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(dollarSigns);
+            if (restaurant is null)
+            {
+                return NotFound();
+            }
+            return Ok(restaurant);
+        }
+
+        //PUT (Update)
+
+        //DELETE BY ID
     }
 }
